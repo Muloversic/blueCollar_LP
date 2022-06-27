e9f65ea5f25a643064430cf6b8aa9b57e6c12fe2
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-06-27 15:15:02 +0300

    created burger menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './styles/reset.css';
 import './styles/icon-font.css';
 import './styles/main.scss';
+
+const burgerIcon = document.querySelector('.header__burger-icon');
+const burgerBody = document.querySelector('.header__burger-menu');
+const burgerParent = document.querySelector('.header__socials-wrapper');
+
+burgerIcon.addEventListener('click', () => {
+  const shiftWidth = document.querySelector('.header__burger-menu').clientWidth;
+  burgerBody.classList.toggle('header__burger-menu--active');
+  burgerIcon.classList.toggle('icon-cross');
+  document.body.classList.toggle('lock');
+  if ([...burgerBody.classList].includes('header__burger-menu--active')) {
+    burgerParent.style.left = shiftWidth + 'px';
+  } else {
+    burgerParent.style.left = 0 + 'px';
+  }
+});
