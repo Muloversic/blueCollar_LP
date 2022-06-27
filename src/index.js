00c6c43a4004dd1035e7d6fe066f5ed5ff159515
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-06-27 16:44:17 +0300

    added burger wrapper and fixed burger position

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import './styles/reset.css';
 import './styles/icon-font.css';
 import './styles/main.scss';
 
+const burgerWrapper = document.querySelector('.header__burger-wrapper');
 const burgerIcon = document.querySelector('.header__burger-icon');
-const burgerBody = document.querySelector('.header__burger-menu');
-const burgerParent = document.querySelector('.header__socials-wrapper');
-
+const socialHeaderWrapepr = document.querySelector('.header__socials-wrapper');
 burgerIcon.addEventListener('click', () => {
   const shiftWidth = document.querySelector('.header__burger-menu').clientWidth;
-  burgerBody.classList.toggle('header__burger-menu--active');
+  burgerWrapper.classList.toggle('header__burger-wrapper--active');
   burgerIcon.classList.toggle('icon-cross');
   document.body.classList.toggle('lock');
-  if ([...burgerBody.classList].includes('header__burger-menu--active')) {
-    burgerParent.style.left = shiftWidth + 'px';
+  if ([...burgerWrapper.classList].includes('header__burger-wrapper--active')) {
+    socialHeaderWrapepr.style.left = shiftWidth + 'px';
   } else {
-    burgerParent.style.left = 0 + 'px';
+    socialHeaderWrapepr.style.left = 0 + 'px';
   }
 });
